e78a7ccabcef42f4e6f0e5673fa2c4450f9d6183
amshepard <antoineshepard@pursuit.org>
2023-05-24 23:32:37 -0400

    Left this open and need to commit. Will return.

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+
+  const [employees, setEmployees] = useState([]);
+  
+
+  useEffect(() => {
+    fetch(`https://resource-veterinarian-api.fly.dev/api/employees`)
+      .then((response) => response.json())
+      .then((response) => {
+        // console.log("It worked!")
+        // console.log(response)
+        setEmployees(response);
+      })
+  }, []) 
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map(employee => {
+          return (
+            <Employee key={employee.id} employee={employee} />
+          )
+        })}
       </section>
     </main>
   );
